f0e54c1da6f06e5341f5f1510d86a4147aac4713
Vital Fadeev <vital.fadeev@gmail.com>
2025-07-21 06:38:50 +0700

    upd

--- source/world.d
+++ source/world.d
@@ -10,135 +10,15 @@ World {
     Widgets    widgets;
     // Worlds
     World*     next;
-
-    /*
-    auto
-    see (World.Event* event) {
-        // Grid able
-        //   сетка матчит по сеточным координатам
-        //     сеточные координаты лежат в event, туда попадают из конвертора
-        //   pointer events
-        //     motion
-        //     button
-        // Hot key
-        //   key events
-        // World events
-
-        // key
-        //   -> focused
-        // pointer
-        //   -> widgets
-
-
-        // Grid able
-        //   сначала верхнй мир
-        //   затем нижний мир
-        //     для решения "widget поверх мир"
-
-        if (event.is_widgetable)
-        if (event.is_gridable)
-        foreach (widget; widgets.walk) {
-            if (widget.grid.match (event.grid.loc)) {
-                event.widget.widget = widget;
-            }
-        }
-
-        return World.Event ();
-    }
-    */
-
-    void
-    rasterize (Len,FILL_FN) (Len window_len, FILL_FN fill) {
-        // min_loc -> window coord
-        version (NEVER) {
-            auto kx = 1366 / L.max;  // 1024  // бижайшее цело степень двойки
-            auto ky =  768 / L.max;  //  512  // бижайшее цело степень двойки
-                                     //       // хвосты влево и вправо
-
-             auto wind_x = near_2_int (window_len.x);
-             auto grid_x = near_2_int (L.max);
-             auto rest_x = wind_x - grid_x;  //хвосты влево и вправо
-             auto padl_x = rest_x / 2; 
-
-             // на сколько сдвигать биты ?
-             auto wind_x_msb = msb (window_len.x);
-             auto grid_x_msb = msb (L.max);
-
-             int shift;
-             int shift_left;
-             if (wind_x_msb > grid_x_msb) {
-                shift      = (wind_x_msb - grid_x_msb);
-                shift_left = true;
-            }
-            else {
-                shift      = (grid_x_msb - wind_x_msb);
-                shift_left = false;
-            }
-        }
-
-        foreach (_widget; widgets) {  // SIMD
-            // fast vetsion
-            auto min_loc = _widget.min_loc;
-            auto max_loc = _widget.max_loc;
-            if (shift_left) {
-                auto windowed_min_x = min_loc.x << shift;
-                auto windowed_min_y = min_loc.y << shift;
-                auto windowed_max_x = max_loc.x << shift;
-                auto windowed_max_y = max_loc.y << shift;
-
-                fill (
-                    padl_x + windowed_min_x, windowed_min_y,
-                    padl_x + windowed_max_x, windowed_max_y);
-            }
-            else {
-                auto windowed_min_x = min_loc.x >> shift;
-                auto windowed_min_y = min_loc.y >> shift;
-                auto windowed_max_x = max_loc.x >> shift;
-                auto windowed_max_y = max_loc.y >> shift;
-
-                fill (
-                    padl_x + windowed_min_x, windowed_min_y,
-                    padl_x + windowed_max_x, windowed_max_y);
-            }
-            // slow version
-            version (NEVER) {
-            auto min_loc = _widget.min_loc;
-            auto max_loc = _widget.max_loc;
-            auto windowed_min_x = min_loc.x * window_len.x / L.max;
-            auto windowed_min_y = min_loc.y * window_len.y / L.max;
-            auto windowed_max_x = max_loc.x * window_len.x / L.max;
-            auto windowed_max_y = max_loc.y * window_len.y / L.max;
-            }
-        }
-    }
-
-    Widget*
-    opOpAssign (string op : "~") (Widget* b) {
-        widgets ~= b;
-        return b;
-    }
 }
 
 struct
 Container {
-    mixin ListAble!(typeof(this)) list;
-    mixin WalkAble!(typeof(this)) walk;
-    Grid.Widget grid;
-    mixin ContAble!(typeof(this)) cont;
+    mixin ListAble list;
+    mixin WalkAble walk;
+    mixin GridAble grid;
+    mixin ContAble cont;
 
-    version (NEVER) {
-    // Containers DList
-    Container* l;
-    Container* r;
-    // Able
-    bool       able = true;
-    // Grid                // Сеточные координаты
-    Loc        min_loc;    // начало, включая границу
-    Loc        max_loc;    // конец, включая границу
-    // Container
-    Container* container;  // id контейнера = указатель
-    Len        fix_len;    // fixed len, in gris-coord, 0 = auto
-    }
     // Container algo
     Way        way     = Way.r;
     Balance    balance = Balance.l;
@@ -165,19 +45,26 @@ Container {
     }
 }
 
-struct
-Hbox_Container {
-    Container _super;
-    alias _super this;
+mixin template
+ContAble () {
+    // Container           // Контейнерные кооринаты
+    Container* container;  // id контейнера = указатель
+    Len        fix_len;    // fixed len, in gris-coord, 0 = auto    
 }
 
 struct
 Containers {  // DList
-    Container* l;
-    Container* r;
+    Walk!Container s;
+    alias s this;
+}
 
-    Container*
-    opOpAssign (string op : "~") (Container* b) {
+struct
+List (T) {
+    T* l;  // start  // first
+    T* r;  // end    // last
+
+    T*
+    opOpAssign (string op : "~") (T* b) {
         if (this.l is null)
             this.l = b;
         else 
@@ -189,40 +76,86 @@ Containers {  // DList
     }
 
     auto
-    walk () {
-        return l.walk.walk (this.l);
+    opSlice () {
+        return Range (this.l);
     }
 
     pragma (inline,true)
     static
     void
-    link (Container* a, Container* b) {
+    link (T* a, T* b) {
         b.l = a;
         a.r = b;
     }
+
+    struct
+    Range {
+        T*   front;
+        bool empty    () { return front is null; }
+        void popFront () {        front = front.r; }
+    }
 }
+
+mixin template
+ListAble () {
+    alias T = typeof(this);
+
+    // Widgets DList
+    T* l;
+    T* r;
+}
+
+
+struct
+Walk (T) {
+    List!T s;
+    alias  s this;
+
+    auto
+    opSlice () {
+        return Filter!"front.walk.able" (s[]);
+    }
+
+    T*
+    opOpAssign (string op : "~") (T* b) {
+        s ~= b;
+        return b;
+    }
+}
+
+auto
+Filter (string COND,R) (R range) {        
+    struct
+    _Filter (R) {
+        R range;
+
+              this      (R range) { this.range = range; _find_able (); }
+        auto  front     () { return range.front; }
+        auto  empty     () { return range.empty; }
+        void  popFront  () {        range.popFront; _find_able (); }
+        void _find_able () {        while (!empty && !mixin (COND)) range.popFront (); }
+    }
+
+    return _Filter!R (range);
+}
+
+
+mixin template
+WalkAble () {
+    // List-based
+    alias T = typeof(this);
+
+    // Able
+    bool able = true;
+}
+
 
 struct
 Widget {
-    mixin ListAble!(typeof(this)) list;
-    mixin WalkAble!(typeof(this)) walk;
-    Grid.Widget grid;
-    mixin ContAble!(typeof(this)) cont;
-    //mixin EvntAble!(typeof(this)) evnt;
-    
-    version (NEVER) {
-    // DList
-    Widget*    l;
-    Widget*    r;
-    // Walk
-    bool       walk_able = true;
-    // Grid                // Сеточные координаты
-    Loc        min_loc;    // начало, включая границу
-    Loc        max_loc;    // конец, включая границу
-    // Container           // Контейнерные кооринаты
-    Container* container;  // id контейнера = указатель
-    Len        fix_len;    // fixed len, in gris-coord, 0 = auto
-    }
+    mixin ListAble list;
+    mixin WalkAble walk;
+    mixin GridAble grid;
+    mixin ContAble cont;
 
     this (Loc min_loc, Loc max_loc) {
         this.grid.min_loc = min_loc;
@@ -230,97 +163,20 @@ Widget {
     }
 
     this (Container* container, Len fix_len) {
-        this.container = container;
-        this.fix_len   = fix_len;        
+        this.cont.container = container;
+        this.cont.fix_len   = fix_len;        
     }
 }
 
-mixin template
-ListAble (T) {
-    // Widgets DList
-    T* l;
-    T* r;
-
-    auto
-    walk (T* start) {
-        return Walker (start);
-    }
-
-    struct
-    Walker {
-        T*   front;
-        bool empty    () { return (front is null); }
-        void popFront () { front = front.r; }
-    }
-}
-
-mixin template
-WalkAble (T) {
-    // DList-based
-
-    // Able
-    bool able = true;
-
-    auto
-    walk (T* start) {
-        return Walker (start);
-    }
-
-    struct
-    Walker {
-        T*    front;
-              this      (T* front) { this.front = front; if (!empty && !_able) _find_able (); }
-        bool  empty     () { return (front is null); }
-        void  popFront  () { _find_able (); }
-        void _find_able () { do front = front.r; while (!empty && !_able); }
-        bool _able      () { return front.able; }
-    }
-}
-
-
-mixin template
-ContAble (T) {
-    // Container           // Контейнерные кооринаты
-    Container* container;  // id контейнера = указатель
-    Len        fix_len;    // fixed len, in gris-coord, 0 = auto    
-}
 
 struct
 Widgets {  // DList
-    Widget* l;
-    Widget* r;
+    Walk!Widget s;
+    alias s this;
 
-    pragma (inline,true)
-    static
-    void
-    link (Widget* a, Widget* b) {
-        b.l = a;
-        a.r = b;
-    }
-
-    Widget*
-    opOpAssign (string op : "~") (Widget* b) {
-        if (this.l is null)
-            this.l = b;
-        else
-            link (this.r, b);
-
-        this.r = b;
-
-        return b;
-    }
-
-    auto
-    opCall () {
-        return l.walk.walk (this.l);
-    }
     auto
     opCall (Grid.Loc loc) {
-        return l.walk.walk (this.l);
-    }
-    auto
-    walk () {
-        return l.walk.walk (this.l);
+        return s[];
     }
 }
 
@@ -339,19 +195,17 @@ Grid {  // SIMD
             return true;
         return false;
     }
+}
 
-    struct
-    Widget {
-        import loc;
-        
-        // Grid                // Сеточные координаты
-        Loc        min_loc;    // начало, включая границу
-        Loc        max_loc;    // конец, включая границу    
+mixin template
+GridAble () {
+    // Grid       // Сеточные координаты
+    Grid.Loc min_loc;  // начало, включая границу
+    Grid.Loc max_loc;  // конец, включая границу    
 
-        bool
-        match (Loc loc) {
-            return Grid.between (loc, min_loc,max_loc);
-        }
+    bool
+    match (Grid.Loc loc) {
+        return Grid.between (loc, min_loc,max_loc);
     }
 }
 
